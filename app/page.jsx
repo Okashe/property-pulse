c1918dcc89fb8fadb90d6d999a477d6375803f1f
Okashe <okashabdi88@gmail.com>
2024-05-06 17:28:22 -0500

    use helpful hooks

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
-const page = () => {
+const HomePage = () => {
   return (
     <div>
-      Home Page
+       <h1  className='text-3xl'>Welcome</h1>
+       <a href="/properties">Show Properties</a>
     </div>
   )
 }
 
-export default page
+export default HomePage
