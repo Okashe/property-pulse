63f0dcf90118764d5c2b0483d0c482c8b3bd5e28
Okashe <okashabdi88@gmail.com>
2024-05-08 00:30:04 -0500

    add notfound and loading pages

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 import Navbar from '@/components/Navbar'
 import Hero from '@/components/Hero'
 import InfoBoxes from '@/components/InfoBoxes'
+import HomeProperties from '@/components/HomeProperties'
 
 
 const HomePage = () => {
@@ -8,6 +9,7 @@ const HomePage = () => {
     <>
         <Hero/>
         <InfoBoxes/>
+        <HomeProperties/>
     </>
   )
 }
--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -5,11 +5,13 @@ const PropertiesPage = () => {
   return (
     <section className='px-4 py-4'>
        <div className='container-xl lg:container m-auto px-4 py-6'>
-         {properties.length === 0 ? (
+         {
+           properties.length === 0 ? (
            <p>No properties found!</p>
          ):(
-          <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-          {properties.map((property)=>(
+          <div className='grid grid-cols-1 md:grid-cols-4 gap-6'>
+          {
+            properties.map((property)=>(
              <PropertyCard 
                key={property._id}
                property={property}
