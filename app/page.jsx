59fb120898a99bd79fed3391bc07c1c2303bbb98
Okashe <okashabdi88@gmail.com>
2024-05-08 15:18:50 -0500

    fetch properties from database

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,9 @@
 import Hero from '@/components/Hero'
 import InfoBoxes from '@/components/InfoBoxes'
 import HomeProperties from '@/components/HomeProperties'
-import connectDB from '@/config/database'
 
-const HomePage = async() => {
-  await connectDB();
+
+const HomePage = () => {
 
   return (
     <>
